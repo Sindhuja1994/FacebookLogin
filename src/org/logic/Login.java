532b5fcbea357ca77f96e037cce30311581290fa
Sindhuja1994 <sindhu.16.arj@gmail.com>
2021-11-09 12:20:11 +0530

    User Functionality

--- src/org/logic/Login.java
+++ src/org/logic/Login.java
@@ -2,4 +2,12 @@ package org.logic;
 
 public class Login {
 
+	public static void username() {
+		System.out.println("Username");
+	}
+
+	public static void main(String[] args) {
+		username();
+	}
+
 }
--- src/org/pojo/Pojo.java
+++ src/org/pojo/Pojo.java
@@ -1,5 +1,14 @@
 package org.pojo;
 
 public class Pojo {
+	
+	public static void password()
+	{
+		System.out.println("Password");
+	}
 
+	public static void main(String[] args) {
+		password();
+	}
+	
 }
